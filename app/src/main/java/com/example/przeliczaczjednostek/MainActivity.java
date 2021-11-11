9132581a1e477bac4c37d9c16cabdc01e1840a97
Lutencjusz <lutencjusz@gmail.com>
2021-11-11 15:32:06 +0100

    standard aplikacji

---
+++ app/src/main/java/com/example/przeliczaczjednostek/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.przeliczaczjednostek;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
