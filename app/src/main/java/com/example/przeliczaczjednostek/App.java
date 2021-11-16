a9fd13ce0bb66d5bf6678e46e23d09595e28cc3b
Lutencjusz <lutencjusz@gmail.com>
2021-11-16 07:22:18 +0100

    usunięcie zbędnych elementów

--- app/src/main/java/com/example/przeliczaczjednostek/App.java
+++ app/src/main/java/com/example/przeliczaczjednostek/App.java
@@ -2,7 +2,6 @@ package com.example.przeliczaczjednostek;
 
 import android.app.Application;
 import android.content.Context;
-import com.squareup.otto.Bus;
 
 public class App extends Application {
 
